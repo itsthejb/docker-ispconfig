4b6c19be7314d157be796d9531ca43f4bbb50f33
unimock <unimock@gate-m.de>
2016-10-03 18:43:49 +0200

    Minor bugfixes.

--- build/bin/clamav-daemon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p /var/run/clamav/ && chown clamav:clamav /var/run/clamav
-mkdir -p /var/lib/clamav/
-chown -R clamav:clamav /var/lib/clamav/
-
-/usr/sbin/clamd -c /etc/clamav/clamd.conf
-
