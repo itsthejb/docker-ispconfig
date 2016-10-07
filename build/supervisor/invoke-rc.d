4acda05c189facffc6e552a58a7ce144335ddb64
unimock <unimock@gate-m.de>
2016-10-08 00:03:50 +0200

    Cleanup supervisor environment.

---
+++ build/supervisor/invoke-rc.d
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/etc/init.d/${1} ${2}
--- build/supervisor/super-init.sh
+++ build/supervisor/super-init.sh
@@ -4,7 +4,11 @@ SVC=`basename $0`
 
 if [ "$1" = "reload" ] ; then
   /usr/bin/supervisorctl stop  ${SVC}
+  test -x /etc/supervisor/init.d/${SVC} && /etc/supervisor/init.d/${SVC} 
   /usr/bin/supervisorctl start ${SVC}
+elif [ "$1" = "start" ] ; then
+  test -x /etc/supervisor/init.d/${SVC} && /etc/supervisor/init.d/${SVC} 
+  /usr/bin/supervisorctl $1 ${SVC}
 else
   /usr/bin/supervisorctl $1 ${SVC}
 fi
