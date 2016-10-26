1d82cf7b60fb2b0e15e1f1571062c7f67767c0cb
unimock <unimock@gate-m.de>
2016-10-26 15:42:46 +0200

    Integrate rsnapshot. Migrate without_wait_for_resync.

--- build/bin/wait_for_resync.sh
+++ build/bin/wait_for_resync.sh
@@ -17,9 +17,9 @@ while true ; do
   anz=`mysql -s -uroot -p${MYSQL_ROOT_PW} -e "select count(status) from dbispconfig.sys_datalog where  datalog_id > $xxx ; "`
   if [ "$anz" != 0 ] ; then
      echo ""
-     echo "Changes detected. Wait 20s before starting server.sh."
+     echo "Changes detected. Wait 10s before starting server.sh."
      echo ""
-     sleep 20
+     sleep 10
      /usr/local/ispconfig/server/server.sh
      break ;
   fi
