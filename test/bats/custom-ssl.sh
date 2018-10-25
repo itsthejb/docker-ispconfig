ddc3f5c615d8ceb5e9b5ddd56b9f0c36fb616117
Jonathan Crooke <jonathan.crooke@zalando.de>
2018-10-25 10:51:33 +0200

    Fix postfix and dovecot TLS

--- test/bats/custom-ssl.sh
+++ test/bats/custom-ssl.sh
@@ -7,11 +7,30 @@ setup() {
   waitForUp
 }
 
-@test "custom ssl certificate" {
+@test "ispconfig uses custom ssl certificate" {
   run openssl s_client -connect $CONTAINER:8080 \
     -cert "$SSL_CERT" \
     -key "$SSL_KEY" \
     -state -debug  
   [ $(echo "$output" | grep subject | grep "CN=$HOSTNAME") ]
   [ $(echo "$output" | grep issuer | grep "CN=certauthority.com") ]
+}
+
+@test "postfix uses custom ssl certificate" {
+  run openssl s_client -connect $CONTAINER:587 \
+    -starttls smtp \
+    -cert "$SSL_CHAIN" \
+    -key "$SSL_KEY" \
+    -state -debug
+  [ $(echo "$output" | grep subject | grep "CN=$HOSTNAME") ]
+  [ $(echo "$output" | grep issuer | grep "CN=certauthority.com") ]
+}
+
+@test "dovecot uses custom ssl certificate" {
+  run openssl s_client -connect $CONTAINER:993 \
+    -cert "$SSL_CHAIN" \
+    -key "$SSL_KEY" \
+    -state -debug
+  [ $(echo "$output" | grep subject | grep "CN=myhost.$HOSTNAME") ]
+  [ $(echo "$output" | grep issuer | grep "CN=myhost.$HOSTNAME") ]
 }
