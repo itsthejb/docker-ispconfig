c8f4543c34d4257d9a73747d1fa5ca77b2fde98e
Jonathan Crooke <jon.crooke@gmail.com>
2018-12-08 13:18:22 +0100

    Email security and tests

--- test/bats/custom-ssl.sh
+++ test/bats/custom-ssl.sh
@@ -7,21 +7,49 @@ setup() {
   waitForUp
 }
 
+function openSSL() {
+  echo QUIT | openssl s_client "$@" 2>&1
+}
+
 function testSSL() {
   waitForPort $1
-  run openssl s_client -showcerts -connect $CONTAINER:$1 $2 2> /dev/null
+  run openSSL -showcerts -connect $CONTAINER:$1 $2
   [ $(echo "$output" | grep subject | grep "CN=$HOSTNAME") ]
   [ $(echo "$output" | grep issuer | grep "CN=certauthority.com") ]
 }
 
 @test "apache uses custom ssl certificate" {
-  testSSL 443
+  run testSSL 443
 }
 
 @test "ispconfig uses custom ssl certificate" {
-  testSSL 8080
+  run testSSL 8080
 }
 
-@test "postfix uses secure ports" {
-  testSSL 465
+@test "postfix uses custom ssl certificate" {
+  run testSSL 465
+  run testSSL 587 "-starttls smtp"
+}
+
+@test "dovecot uses custom ssl certificate" {
+  run testSSL 110 "-starttls pop3"
+  run testSSL 995
+  run testSSL 143 "-starttls imap"
+  run testSSL 993
+}
+
+@test "dovecot has expected imap login" {
+  run openSSL -connect $CONTAINER:143 -starttls imap
+  [ ! $(echo "$output" | grep "AUTH=login") ]
+  [ ! $(echo "$output" | grep "AUTH=plain") ]
+  run openSSL -connect $CONTAINER:993
+  [ ! $(echo "$output" | grep "AUTH=login") ]
+  [ ! $(echo "$output" | grep "AUTH=plain") ]
+}
+
+@test "roundcube uses tls" {
+  run docker exec ispconfig cat /opt/roundcube/config/config.inc.php
+  [ $(echo "$output" | grep "\$config\['default_host'\] = 'tls://$HOSTNAME';") ]
+  [ $(echo "$output" | grep "\$config\['smtp_server'\] = 'tls://$HOSTNAME';") ]
+  [ $(echo "$output" | grep "\$config\['smtp_port'\] = 587;") ]
 }
